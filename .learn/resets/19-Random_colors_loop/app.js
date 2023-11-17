b67bb449726fbe83fedcd5db403a35270504ee4c
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-11-17 15:33:00 +0000

    [Done]

--- .learn/resets/19-Random_colors_loop/app.js
+++ .learn/resets/19-Random_colors_loop/app.js
@@ -18,10 +18,7 @@ function getColor(colorNumber = 0) {
 function getAllStudentColors() {
 
 	//your loop here
-	for (let i = 0; i < 10; i++){
-	let color = getColor(Math.floor(Math.random() * 4 )+1);
-	console.log(color);
-	}
+	let exampleColor = getColor(1);
 }
 
 //call the function below with the number of students in the class and print on the console
--- exercises/04-User_inputed_variables/app.js
+++ exercises/04-User_inputed_variables/app.js
@@ -1,3 +1,6 @@
 let age = prompt('What is your age?');
 
 // Your code below:
+let added = parseInt(age)+10
+
+console.log(added);
